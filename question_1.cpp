2a9210f343372331b85ace32233fdcad3b071dba
Thien Nguyen <minhthien.k@gmail.com>
2025-10-08 00:30:07 +0700

    fix question 1, reorder the array descending

--- question_1.cpp
+++ question_1.cpp
@@ -1,51 +1,50 @@
-# include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    // Khai báo n và m
-    int n, m;
-    
-    // Nhập n
-    cout << "Nhập số lượng phần tử trong mảng: ";
-    cin >> n;
-    
-    // Nhập mảng
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Nhập phần tử số " << i + 1 << ": ";
-        cin >> arr[i];
-    }
+  // Khai báo n và m
+  int n, m;
 
-    // Nhập m, không biết dùng từ tiếng việt mô tả số m này như nào luôn thầy
-    cout << "Nhập thứ tự số lớn nhất: ";
-    cin >> m;
+  // Nhập n
+  cout << "Nhập số lượng phần tử trong mảng: ";
+  cin >> n;
 
-    // Sắp xếp mảng
-    for (int i = 0; i < n; i++)
-    {
-      int minIndex = i;
-      for (int j = i + 1; j < n; j++)
-      {
-        if (arr[j] < arr[minIndex])
-        {
-          minIndex = j;
-        }
-      }
-      int temp = arr[i];
-      arr[i] = arr[minIndex];
-      arr[minIndex] = temp;
-    }
-
-    // In ra mảng để kiểm tra
-    for (int i = 0; i < n; i++)
-    {
-      cout << arr[i] << " ";
-    }
-
-    // Số lớn thứ m
-    cout << "\nSố lớn thứ " << m << " là " << arr[m - 1];
+  // Nhập mảng
+  int arr[n];
+  for (int i = 0; i < n; i++)
+  {
+    cout << "Nhập phần tử số " << i + 1 << ": ";
+    cin >> arr[i];
   }
 
+  // Nhập m, không biết dùng từ tiếng việt mô tả số m này như nào luôn thầy
+  cout << "Nhập thứ tự số lớn nhất: ";
+  cin >> m;
+
+  // Sắp xếp mảng
+  for (int i = 0; i < n; i++)
+  {
+    int maxIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+      if (arr[j] > arr[maxIndex])
+      {
+        maxIndex = j;
+      }
+    }
+    int temp = arr[i];
+    arr[i] = arr[maxIndex];
+    arr[maxIndex] = temp;
+  }
+
+  // In ra mảng để kiểm tra
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+
+  // Số lớn thứ m
+  cout << "\nSố lớn thứ " << m << " là " << arr[m - 1];
+}
