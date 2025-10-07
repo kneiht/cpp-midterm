0de96d36cb67b68e1f2ed1cc296d631af206efd3
Thien Nguyen <minhthien.k@gmail.com>
2025-10-08 00:06:17 +0700

    handle 1 student, refactor later

--- question_2.cpp
+++ question_2.cpp
@@ -63,3 +63,65 @@ public:
         }
     }
 };
+
+int main()
+{
+    string maSinhVien;
+    string tenSinhVien;
+    // Nhập 1 sinh viên
+    cout << "Nhập thông tin sinh viên\n";
+    cout << "Nhập mã sinh viên: ";
+    cin >> maSinhVien;
+    cout << "Nhập tên sinh viên: ";
+    cin >> tenSinhVien;
+    cout << "\n";
+
+    // Môn toán
+    int diemToan;
+    cout << "Nhập điểm môn Toán: ";
+    cin >> diemToan;
+    MonHoc toan = MonHoc(1, "Toán", diemToan);
+
+    int diemLy;
+    cout << "Nhập điểm môn Lý: ";
+    cin >> diemLy;
+    MonHoc ly = MonHoc(2, "Lý", diemLy);
+
+    int diemHoa;
+    cout << "Nhập điểm môn Hóa: ";
+    cin >> diemHoa;
+    MonHoc hoa = MonHoc(3, "Hóa", diemHoa);
+
+    int diemVan;
+    cout << "Nhập điểm môn Văn: ";
+    cin >> diemVan;
+    MonHoc van = MonHoc(4, "Văn", diemVan);
+
+    int diemSu;
+    cout << "Nhập điểm môn Sử: ";
+    cin >> diemSu;
+    MonHoc su = MonHoc(5, "Sử", diemSu);
+
+    int diemDia;
+    cout << "Nhập điểm môn Địa: ";
+    cin >> diemDia;
+    MonHoc dia = MonHoc(6, "Địa", diemDia);
+
+    // Tạo sinh viên
+    MonHoc monHoc[6] = {toan, ly, hoa, van, su, dia};
+    SinhVien sv = SinhVien(maSinhVien, tenSinhVien, monHoc);
+
+    // In ra sinh viên
+    cout << "\n\n"
+         << "Thông tin sinh viên\n"
+         << "Mã sinh viên: " << maSinhVien << "\n"
+         << "Tên sinh viên: " << tenSinhVien << "\n"
+         << "\n"
+         << "Điểm môn Toán: " << toan.layDiemMonHoc() << "\n"
+         << "Điểm môn Lý: " << ly.layDiemMonHoc() << "\n"
+         << "Điểm môn Hóa: " << hoa.layDiemMonHoc() << "\n"
+         << "Điểm môn Văn: " << van.layDiemMonHoc() << "\n"
+         << "Điểm môn Sử: " << su.layDiemMonHoc() << "\n"
+         << "Điểm môn Địa: " << dia.layDiemMonHoc() << "\n"
+         << "\n";
+}
