74687f84e346767a0f76e2bfcf212ce5cffe9f75
Thien Nguyen <minhthien.k@gmail.com>
2025-10-08 00:13:05 +0700

    complete question 2

--- question_2.cpp
+++ question_2.cpp
@@ -52,6 +52,17 @@ public:
     string maSinhVien;
     string tenSinhVien;
 
+    // Hàm khởi tạo mặc định
+    SinhVien()
+    {
+        maSinhVien = "";
+        tenSinhVien = "";
+        for (int i = 0; i < 6; i++)
+        {
+            monHoc[i] = MonHoc();
+        }
+    }
+
     // Hàm khởi tạo
     SinhVien(string maSinhVien, string tenSinhVien, MonHoc monHoc[6])
     {
@@ -62,66 +73,85 @@ public:
             this->monHoc[i] = MonHoc(monHoc[i].layMaMonHoc(), monHoc[i].layTenMonHoc(), monHoc[i].layDiemMonHoc());
         }
     }
+
+    // In ra sinh viên
+    void inSinhVien()
+    {
+        // In ra sinh viên
+        cout << "\n\n"
+             << "Thông tin sinh viên\n"
+             << "Mã sinh viên: " << maSinhVien << "\n"
+             << "Tên sinh viên: " << tenSinhVien << "\n"
+             << "\n"
+             << "Điểm môn Toán: " << monHoc[0].layDiemMonHoc() << "\n"
+             << "Điểm môn Lý: " << monHoc[1].layDiemMonHoc() << "\n"
+             << "Điểm môn Hóa: " << monHoc[2].layDiemMonHoc() << "\n"
+             << "Điểm môn Văn: " << monHoc[3].layDiemMonHoc() << "\n"
+             << "Điểm môn Sử: " << monHoc[4].layDiemMonHoc() << "\n"
+             << "Điểm môn Địa: " << monHoc[5].layDiemMonHoc() << "\n"
+             << "\n";
+    };
 };
 
 int main()
 {
-    string maSinhVien;
-    string tenSinhVien;
-    // Nhập 1 sinh viên
-    cout << "Nhập thông tin sinh viên\n";
-    cout << "Nhập mã sinh viên: ";
-    cin >> maSinhVien;
-    cout << "Nhập tên sinh viên: ";
-    cin >> tenSinhVien;
-    cout << "\n";
+    int soLuong;
+    cout << "Nhập số lượng sinh viên: ";
+    cin >> soLuong;
+    SinhVien sv[soLuong];
 
-    // Môn toán
-    int diemToan;
-    cout << "Nhập điểm môn Toán: ";
-    cin >> diemToan;
-    MonHoc toan = MonHoc(1, "Toán", diemToan);
+    for (int i = 0; i < soLuong; i++)
+    {
+        string maSinhVien;
+        string tenSinhVien;
+        // Nhập 1 sinh viên
+        cout << "Nhập thông tin sinh viên\n";
+        cout << "Nhập mã sinh viên: ";
+        cin >> maSinhVien;
+        cout << "Nhập tên sinh viên: ";
+        cin >> tenSinhVien;
+        cout << "\n";
 
-    int diemLy;
-    cout << "Nhập điểm môn Lý: ";
-    cin >> diemLy;
-    MonHoc ly = MonHoc(2, "Lý", diemLy);
+        // Môn toán
+        int diemToan;
+        cout << "Nhập điểm môn Toán: ";
+        cin >> diemToan;
+        MonHoc toan = MonHoc(1, "Toán", diemToan);
 
-    int diemHoa;
-    cout << "Nhập điểm môn Hóa: ";
-    cin >> diemHoa;
-    MonHoc hoa = MonHoc(3, "Hóa", diemHoa);
+        int diemLy;
+        cout << "Nhập điểm môn Lý: ";
+        cin >> diemLy;
+        MonHoc ly = MonHoc(2, "Lý", diemLy);
 
-    int diemVan;
-    cout << "Nhập điểm môn Văn: ";
-    cin >> diemVan;
-    MonHoc van = MonHoc(4, "Văn", diemVan);
+        int diemHoa;
+        cout << "Nhập điểm môn Hóa: ";
+        cin >> diemHoa;
+        MonHoc hoa = MonHoc(3, "Hóa", diemHoa);
 
-    int diemSu;
-    cout << "Nhập điểm môn Sử: ";
-    cin >> diemSu;
-    MonHoc su = MonHoc(5, "Sử", diemSu);
+        int diemVan;
+        cout << "Nhập điểm môn Văn: ";
+        cin >> diemVan;
+        MonHoc van = MonHoc(4, "Văn", diemVan);
 
-    int diemDia;
-    cout << "Nhập điểm môn Địa: ";
-    cin >> diemDia;
-    MonHoc dia = MonHoc(6, "Địa", diemDia);
+        int diemSu;
+        cout << "Nhập điểm môn Sử: ";
+        cin >> diemSu;
+        MonHoc su = MonHoc(5, "Sử", diemSu);
 
-    // Tạo sinh viên
-    MonHoc monHoc[6] = {toan, ly, hoa, van, su, dia};
-    SinhVien sv = SinhVien(maSinhVien, tenSinhVien, monHoc);
+        int diemDia;
+        cout << "Nhập điểm môn Địa: ";
+        cin >> diemDia;
+        MonHoc dia = MonHoc(6, "Địa", diemDia);
 
-    // In ra sinh viên
-    cout << "\n\n"
-         << "Thông tin sinh viên\n"
-         << "Mã sinh viên: " << maSinhVien << "\n"
-         << "Tên sinh viên: " << tenSinhVien << "\n"
-         << "\n"
-         << "Điểm môn Toán: " << toan.layDiemMonHoc() << "\n"
-         << "Điểm môn Lý: " << ly.layDiemMonHoc() << "\n"
-         << "Điểm môn Hóa: " << hoa.layDiemMonHoc() << "\n"
-         << "Điểm môn Văn: " << van.layDiemMonHoc() << "\n"
-         << "Điểm môn Sử: " << su.layDiemMonHoc() << "\n"
-         << "Điểm môn Địa: " << dia.layDiemMonHoc() << "\n"
-         << "\n";
+        // Tạo sinh viên
+        MonHoc monHoc[6] = {toan, ly, hoa, van, su, dia};
+        sv[i] = SinhVien(maSinhVien, tenSinhVien, monHoc);
+    }
+
+    // In ra danh sách sinh viên
+    cout << "\n\n===== Danh sách sinh viên =====\n";
+    for (int i = 0; i < soLuong; i++)
+    {
+        sv[i].inSinhVien();
+    }
 }
